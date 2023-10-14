26521207bdaa3bbcc4f72531e1ad8ce1b2861297
4IMS <nasuschnyj@gmail.com>
2023-10-14 21:45:33 +0300

    correction

--- script/script.js
+++ script/script.js
@@ -1,15 +1,15 @@
-var swiper = new Swiper('.mySwiper', {
+var swiper = new Swiper('.comments-swiper', {
 	slidesPerView: 1,
 	spaceBetween: 16,
 	slidesPerView: 'auto',
 	centeredSlides: true,
 	grabCursor: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
 	pagination: {
 		el: '.swiper-pagination',
 		clickable: true,
-	},
-	navigation: {
-		nextEl: '.swiper-arrow-next',
-		prevEl: '.swiper-arrow-prev',
 	},
 });
